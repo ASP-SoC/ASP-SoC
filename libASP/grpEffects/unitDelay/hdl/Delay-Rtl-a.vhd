3a6da4f65503c94bc07e7fecfde73e70312afd5f
nervid <d.haberleitner@gmail.com>
2017-11-07 15:27:09 +0100

    implemented delay using RAM

--- libASP/grpEffects/unitDelay/hdl/Delay-Rtl-a.vhd
+++ libASP/grpEffects/unitDelay/hdl/Delay-Rtl-a.vhd
@@ -6,7 +6,9 @@
 --               delay can be configured separately.
 -------------------------------------------------------------------------------
 
-architecture Rtl of Template is
+
+
+architecture Rtl of Delay is
 
   ---------------------------------------------------------------------------
   -- Wires
@@ -15,19 +17,21 @@ architecture Rtl of Template is
   ---------------------------------------------------------------------------
   -- Registers
   ---------------------------------------------------------------------------
-  type aConfigReg is array (0 to 1) of unsigned(31 downto 0);
-  signal ConfigReg : aConfigReg := (others => (others => '0'));
+  signal DelayValReg : unsigned(LogDualis(gMaxDelay)-1 downto 0);
 
+  type aMemory is array (0 to gMaxDelay) of std_ulogic_vector(asi_data'range);
+  signal ramBlock : aMemory := (others => (others => '0'));
+
+  subtype aRamAddress is integer range 0 to gDelay;
+  signal readAddress, writeAddress : aRamAddress;
+
+  
 begin
 
   ---------------------------------------------------------------------------
   -- Outputs
   ---------------------------------------------------------------------------
-  aso_left_data   <= asi_left_data;
-  aso_left_valid  <= asi_left_valid;
-  aso_right_data  <= asi_right_data;
-  aso_right_valid <= asi_right_valid;
-
+  
   ---------------------------------------------------------------------------
   -- Signal assignments
   ---------------------------------------------------------------------------
@@ -35,14 +39,62 @@ begin
   ---------------------------------------------------------------------------
   -- Logic
   ---------------------------------------------------------------------------
+  
+
+  ------------------------------------------------------------------------------
+  -- Read and write RAM
+  ------------------------------------------------------------------------------
+  ram_rw : process (iClk) is
+  begin  -- process ram_rw
+    if rising_edge(iClk) then
+      if asi_valid = '1' then
+        ramBlock(writeAddress) <= iDdry;
+      end if;
+      oDwet <= ramBlock(readAddress);
+    end if;
+  end process ram_rw;
+
+  
+  ------------------------------------------------------------------------------
+  -- Address pointer logic
+  ------------------------------------------------------------------------------
+  address_p : process (iClk, inResetAsync) is
+  begin  -- process address_p
+    if inResetAsync = not('1') then     -- asynchronous reset (active low)
+      readAddress  <= 1;
+      writeAddress <= 0;
+    elsif rising_edge(iClk) then        -- rising clock edge
+      -- wait until data is valid
+      if asi_valid = '1' then
+
+        -- increase read address
+        if readAddress = gDelay then
+          readAddress <= 0;
+        else
+          readAddress <= readAddress + 1;
+        end if;
+
+        -- increase write address
+        if writeAddress = gDelay then
+          writeAddress <= 0;
+        else
+          writeAddress <= writeAddress + 1;
+        end if;
+
+      end if;
+    end if;
+  end process address_p;
+
+
 
   -- MM INTERFACE for configuration
   SetConfigReg : process (csi_clk) is
   begin
     if rising_edge(csi_clk) then
-      if avs_s0_write = '1' then
-        ConfigReg(to_integer(unsigned(avs_s0_address)))
-          <= unsigned(avs_s0_writedata);
+      if avs_s0_write = '1' then  -- bad implementation (jumps in output signal
+                                  -- and reapeating parts when changing delay)
+        readAddress <= 0;
+        writeAddress <= to_integer(unsigned(avs_s0_writedata(LogDualis(dMaxDelay)-1 downto 0)));
       end if;
     end if;
   end process;

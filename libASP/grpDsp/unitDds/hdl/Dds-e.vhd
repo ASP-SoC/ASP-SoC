af57e8857e319d541ff226511f38bf3d48f3f816
Fx <FxSt@gmx.at>
2017-12-06 22:16:57 +0100

    add ieee_proposed to gitignore and add ieee_proposed to dds entity because quartus could not work with ieee.fixed_pkg

--- libASP/grpDsp/unitDds/hdl/Dds-e.vhd
+++ libASP/grpDsp/unitDds/hdl/Dds-e.vhd
@@ -9,8 +9,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.fixed_float_types.all;
-use ieee.fixed_pkg.all;
+
+library ieee_proposed;
+use ieee_proposed.fixed_pkg.all;
 
 use work.sin_4096.all;
 
